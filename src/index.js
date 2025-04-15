e0019fbbf5eaa2fa1ee471a12dedf7e1530c4ab2
Zaka Ullah <zakaullah.khan@ziscomm.me>
2025-04-15 23:26:14 +0500

    Initial Project setup for car rental system Manzil Drive

--- src/index.js
+++ src/index.js
@@ -1,17 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import React from 'react';\nimport ReactDOM from 'react-dom';\nimport './index.css';\nimport App from './App';\n\nReactDOM.render(\n  <React.StrictMode>\n    <App />\n  </React.StrictMode>,\n  document.getElementById('root')\n);
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}", // Add paths to all your HTML, JS, or other relevant files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
